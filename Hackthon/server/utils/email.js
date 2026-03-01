ec9e4ddb441dc0580c5ed907ddde22b2a8409cd1
Nihal <nihalk9876@gmail.com>
2026-03-01 15:14:20 +0530

    Improve email reliability on production

--- Hackthon/server/utils/email.js
+++ Hackthon/server/utils/email.js
@@ -5,10 +5,15 @@ let transporter;
 const getTransporter = () => {
     if (!transporter) {
         transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         });
     }
